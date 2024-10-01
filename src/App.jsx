6091e695857e2efff062e5b3c97749e1f76360bf
musharruf <mdmusharruf13@gmail.com>
2024-10-01 18:44:49 +0530

    tailwindcss and flaticon package added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
       <h1>swiggy clone app</h1>
+      <p>{import.meta.env.VITE_NAME}</p>
+      <p>{JSON.parse(import.meta.env.VITE_DATA).NAME}</p>
     </>
   );
 }
