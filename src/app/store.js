345d5b51224afeb1015b49c63a267916e88867d3
musharruf <mdmusharruf13@gmail.com>
2024-10-14 12:05:30 +0530

    redux store setup and one slice added

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import restaurantSlice from "../slices/restaurantSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        restaurantSlice: restaurantSlice
+    }
+})
---
+++ src/slices/restaurantSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const restaurantSlice = createSlice({
+    name: "restaurantSlice",
+    initialState: {
+        restaurantsData: []
+    },
+    reducers: {
+        setAllRestaurants: (state, action) => {
+            state.restaurantsData.push(...action.payload);
+        },
+
+    }
+});
+
+export default restaurantSlice.reducer;
+export const { setAllRestaurants, getMenus } = restaurantSlice.actions;
