9cd9893360f93c8a3768bea3277ad81f0de4186e
musharruf <mdmusharruf13@gmail.com>
2024-10-29 14:06:11 +0530

    console logs removed

--- src/utils/filterRestaurant.js
+++ src/utils/filterRestaurant.js
@@ -1,17 +1,17 @@
 export default function filterRestaurant(restaurant, filter) {
     const restInfo = restaurant?.info;
     switch (filter) {
-        case 'Fast Delivery': console.log(restInfo?.sla?.deliveryTime);
+        case 'Fast Delivery':
             return restInfo?.sla?.deliveryTime <= 35;
-        case 'New on Swiggy': console.log(restInfo?.externalRatings?.aggregatedRating?.rating);
+        case 'New on Swiggy':
             return restInfo?.externalRatings?.aggregatedRating?.rating === "--";
-        case 'Pure Veg': console.log(restInfo?.badges?.imageBadges && restInfo?.badges?.imageBadges[0].description);
+        case 'Pure Veg':
             return restInfo?.badges?.imageBadges?.length ? restInfo?.badges?.imageBadges[0]?.description === "pureveg" : false;
-        case 'Offers': console.log(restInfo?.aggregatedDiscountInfoV3);
+        case 'Offers':
             return restInfo?.aggregatedDiscountInfoV3?.header ? true : false;
-        case 'Ratings 4.5+': console.log(restInfo?.avgRating);
+        case 'Ratings 4.5+':
             return restInfo?.avgRating > 4.5;
-        case 'Rs. 300-Rs. 600': console.log((restInfo?.costForTwo).split(" ")[0].slice(1));
+        case 'Rs. 300-Rs. 600':
             return (restInfo?.costForTwo).split(" ")[0].slice(1) >= 300 & (restInfo?.costForTwo).split(" ")[0].slice(1) <= 600;
         default: return true;
     }
